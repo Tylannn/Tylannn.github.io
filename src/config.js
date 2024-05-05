8a182ec851fc5e6c82514f30fc767d37dd841b32
Madlyn <98822818+Tylannn@users.noreply.github.com>
2024-05-05 21:40:55 +0800

    .

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export const PandaConfig = {
+    title: 'Panda Blog',
+    description: 'Panda Blog, a blog powered by Astro',
+    start: '2016',
+    site: 'https://astro-theme-panda.vercel.app/',
+    defaultLocale: 'zh',
+    navbar: [
+        // {title: 'Posts', url: '/'}, // auto generated
+        { title: 'about', url: '/about/' }
+    ],
+    footer: [
+        { title: 'RSS', url: '/rss.xml/' }    ]
+}
---
+++ src/type.d.ts
@@ -0,0 +1,21 @@
+declare module '@assets/*'
+declare module '@components/*'
+declare module '@data/*'
+declare module '@layouts/*'
+declare module '@plugins/*'
+declare module '@styles/*'
+declare module '@utils/*'
+declare module '@content/*'
+
+declare module 'remark-pangu' {
+    const pangu: any
+    export default pangu
+}
+
+
+
+export interface PostFrontMatter {
+    title: string
+    date: string
+    draft: boolean
+}
